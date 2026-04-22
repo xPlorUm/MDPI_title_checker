7dddf74c182850bbf388c9c6d2cb82e47a29b62b
Cedric Tschechtelin <76949850+xPlorUm@users.noreply.github.com>
2026-04-22 22:19:37 +0200

    Added API Endpoint that accepts JSON in the correct format

--- src/MDPI_title_checker/main.py
+++ src/MDPI_title_checker/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class TitleRequest(BaseModel):
+    reference: str
+    other: list[str]
 
-# tells FastAPI that the function below is in charge of handling requests that go to the path / using a get operation
-# when a user get this url then the function below is executed
+# @app.get("/")
+async def root():
+    return {"item_id"}
+
+# when a user calls this url then the function below is executed
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
 
-# @app.get("/")
-# async def read_item():
-#     return {"item_id"}
+# Send a Request body from the client (browser) to my API
+@app.post("/find_similar")
+async def find_similar(request: TitleRequest):
+    return request.reference
 
 
-# API endpoint that receives article titles as JSON object
-# Use pretrained model to compute vectors in embedding space for each input title
-# Use L2 distance or cosine distance in vector space 
-
-# Endpoint returns the titlre that is most similar to the reference title
+# 1) API endpoint that receives article titles as JSON object check
+# 2) Use pretrained model to compute vectors in embedding space for each input title
+# 3) Use L2 distance or cosine distance in vector space 
+# 4) Endpoint returns the title that is most similar to the reference title
 
 
 
